730f3269e44eaa2fc2ecc14935ec88fbd1543a93
David Bohaumilitzky <d.bohaumilitzky@icloud.com>
2021-04-23 11:44:13 +0200

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by David Bohaumilitzky on 23.04.21.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
